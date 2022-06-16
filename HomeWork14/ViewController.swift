85b60fd3df37f3b1c45f7541e09cedf45418ec46
Solovkin Denis <solovkin.d@icloud.com>
2022-06-16 10:31:29 +0700

    Initial Commit

---
+++ HomeWork14/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork14
+//
+//  Created by Denis Solovkin on 16.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
